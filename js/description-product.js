ac094ab183d41039ae27eeb62f47033c79186d40
JonatasAlexandrao <jonatas-flash@hotmail.com>
2020-02-17 18:09:15 -0300

    Terminado estrelas

--- js/description-product.js
+++ js/description-product.js
@@ -7,19 +7,58 @@ no momento q for clicado mostrar um texto na tela
 */
 
 const $heart = window.document.querySelector(".-heart");
-
-
 $heart.addEventListener("click" , handleClick);
 
+//desafio 14
+const $stars = document.querySelectorAll(".star");
+var positionLast = $stars.length -1;
+
+
+$stars.forEach(function($star, key){
+    
+   /* if(key == 0) {
+        $star.addEventListener("click", firstStar);
+    }
+
+    if(key == positionLast) {
+        $star.addEventListener("click", lastStar);
+    }
+
+    if(key > 0 && key < positionLast){
+        $star.addEventListener("click", function() {
+            middleStar(key);
+        });
+    }*/
+    $star.addEventListener("click", function() {
+        middleStar(key);
+    });
+
+});
+
 function handleClick() {
-    $heart.classList.toggle("-active");
+    this.classList.toggle("-active");
 }
 
-
-const $star = document.querySelector(".-star");
-
-$star.addEventListener("click", handleClickStar);
-
-function handleClickStar(){
-    $star.classList.toggle("-active");
+function middleStar(index) {
+    
+    $stars.forEach(function ($star, key){
+        $star.classList.remove("-active");
+        if(key <= index){
+            $star.classList.add("-active");
+        }
+    });     
 }
+/*
+function firstStar(){  
+    $stars.forEach(function ($star){
+        $star.classList.remove("-active");
+    });
+    this.classList.add("-active"); 
+}
+
+function lastStar() {
+    $stars.forEach(function($star) {
+        $star.classList.add("-active");
+    });
+}
+*/
